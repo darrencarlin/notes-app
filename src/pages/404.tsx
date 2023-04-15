4823fbb7da3b89aa09766542aeb8aa585ae3df7c
Darren Carlin <dazftw@gmail.com>
2023-04-15 16:38:03 -0400

    styling 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,11 +1,23 @@
+import Link from "next/link";
+
 export default function Custom404() {
   return (
-    <>
-      <h1>404 - Page Not Found</h1>
-      <p>
-        Your account may have been removed, this typically happens if you do not add
-        any notes.
-      </p>
-    </>
+    <div className="w-screen h-screen bg-gray-900 flex justify-center items-center">
+      <div className="max-w-xl">
+        <h1 className="text-2xl mb-4">404 - Page Not Found</h1>
+        <p className="mb-4">
+          Your account may have been removed, this typically happens if you do not
+          add any notes.
+        </p>
+
+        <p>
+          If this was the case, you can create a new account by going back to the{" "}
+          <Link href="/" className="underline text-blue-400">
+            home page
+          </Link>
+          .
+        </p>
+      </div>
+    </div>
   );
 }
