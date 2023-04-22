887905ec08031a19e312e2d97684d0b80e381b6e
Darren Carlin <dazftw@gmail.com>
2023-04-22 16:27:00 -0400

    test

--- __tests__/components/Input.spec.tsx
+++ __tests__/components/Input.spec.tsx
@@ -1,27 +1,21 @@
 import Input from "@/components/inputs/Input";
-import { render, fireEvent } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 
 describe("Input component", () => {
   it("should render with default props", () => {
-    const { getByTestId } = render(<Input data-testid="input" />);
-    expect(getByTestId("input")).toHaveClass("w-full bg-gray-700 p-4 mb-4");
+    render(<Input data-testid="input" />);
+    expect(screen.getByTestId("input")).toHaveClass("w-full bg-gray-700 p-4 mb-4");
   });
 
   it("should apply custom class name", () => {
-    const { getByTestId } = render(
-      <Input data-testid="input" className="my-custom-class" />
-    );
-    expect(getByTestId("input")).toHaveClass("my-custom-class");
+    render(<Input data-testid="input" className="my-custom-class" />);
+    expect(screen.getByTestId("input")).toHaveClass("my-custom-class");
   });
-
   it("should handle change event", () => {
     const onChange = jest.fn();
-    const { getByTestId } = render(
-      <Input data-testid="input" onChange={onChange} />
-    );
-    const input = getByTestId("input") as HTMLInputElement;
+    render(<Input data-testid="input" onChange={onChange} />);
+    const input = screen.getByTestId("input");
     fireEvent.change(input, { target: { value: "test value" } });
     expect(onChange).toHaveBeenCalledTimes(1);
-    expect(input.value).toBe("test value");
   });
 });
