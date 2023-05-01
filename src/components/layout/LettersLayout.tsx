e148a345770f1092f48eac213be84e09cc748919
Darren Carlin <dazftw@gmail.com>
2023-05-01 15:44:22 -0400

    styling

--- src/components/layout/LettersLayout.tsx
+++ src/components/layout/LettersLayout.tsx
@@ -1,7 +1,7 @@
 import type { FC, ReactNode } from "react";
 
 const LettersLayout: FC<{ children: ReactNode }> = ({ children }) => (
-  <section className="no-scrollbar p-4 bg-gray-900 gap-4 overflow-x-scroll flex lg:flex-col md:justify-between md:overflow-y-scroll max-h-[100dvh]">
+  <section className="fixed bottom-0 left-0 w-screen no-scrollbar p-4 bg-gray-900 gap-4 overflow-x-scroll flex lg:flex-col md:justify-between md:overflow-y-scroll lg:max-h-[100dvh] lg:static lg:w-[50px]">
     {children}
   </section>
 );
